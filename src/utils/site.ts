a32b4691e257aca7d2e744b54ce1985f8c3d0fda
krefitagurja <krefitagurja@gmail.com>
2025-07-14 11:24:41 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Colmek Tobrut Sma';
-export const desk = 'Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma';
-export const url = 'https://colmektobrutsma.pages.dev';
-export const terbit = '2025-06-26T00:01:03Z';
